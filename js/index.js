95d46f8eee184bc2bd710edf3be2fb087ef6e85b
Bujar Tahiri <bujaar.tahiri@gmail.com>
2020-09-24 01:42:47 +0200

    Add todo function

--- js/index.js
+++ js/index.js
@@ -14,3 +14,17 @@ const options = {weekday: 'long', month: 'short', day: 'numeric'}
 const today = new Date()
 dateElement.innerHTML = today.toLocaleDateString('en-US', options)
 
+// add todo
+function addTodo (todo) {
+    const item = `
+        <li class="item">
+            <i class="fa fa-circle-thin co" job="complete" id="0"></i>
+            <p class="text">${ todo }</p>
+            <i class="fa fa-trash-o de" job="delete" id="0"></i>
+        </li>
+    `
+
+    const position = 'beforeend'
+    list.insertAdjacentHTML(position, item)
+}
+addTodo('drink coffee')
